e80ec8acd855365a8b2198e266280dcd4651d653
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2015-03-03 13:49:29 +0100

    First steps for header parsing

--- Sharpineer/Sharpineer/Parser/Header/HeaderParser.cs
+++ Sharpineer/Sharpineer/Parser/Header/HeaderParser.cs
@@ -1,6 +1,54 @@
-﻿namespace Sharpineer.Parser.Header
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ClangSharp;
+
+namespace Sharpineer.Parser.Header
 {
-    class HeaderParser
+    public class HeaderParser
     {
+        /// <summary>
+        /// All header files
+        /// </summary>
+        public readonly string[] HeaderFiles;
+
+        public HeaderParser(params string[] headerFiles)
+        {
+            HeaderFiles = headerFiles;
+        }
+
+        /// <summary>
+        /// Parses all headers using libClang
+        /// </summary>
+        public void Parse()
+        {
+            var createIndex = clang.createIndex(0, 0);
+
+            var args = new List<string> { "-x", "c++", "-I./" };
+
+            var tmpfile = Path.GetTempFileName();
+            var lines = HeaderFiles.Select(header => "#include \"" + header + "\"").ToList();
+            File.WriteAllLines(tmpfile, lines);
+
+            CXTranslationUnit translationUnit;
+            CXUnsavedFile unsavedFile;
+            var translationUnitError = clang.parseTranslationUnit2(createIndex, tmpfile, args.ToArray(), args.Count, out unsavedFile, 0, 0, out translationUnit);
+
+            if (translationUnitError != CXErrorCode.CXError_Success)
+            {
+                Console.WriteLine("Error: " + translationUnitError);
+                var numDiagnostics = clang.getNumDiagnostics(translationUnit);
+
+                for (uint i = 0; i < numDiagnostics; ++i)
+                {
+                    var diagnostic = clang.getDiagnostic(translationUnit, i);
+                    Console.WriteLine(clang.getDiagnosticSpelling(diagnostic).ToString());
+                    clang.disposeDiagnostic(diagnostic);
+                }
+
+                throw new InvalidOperationException("Error while compiling");
+            }
+        }
     }
 }
---
+++ Sharpineer/Sharpineer/data/EnumTestHeader.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+enum FooE
+{
+	Entry1,
+	Entry2,
+	Entry4 = 4
+};
+
+enum class FooEC
+{
+	Val0,
+	Val1,
+	Val2
+};
+
+enum class FooECB : short
+{
+	ShortV0,
+	ShortV1,
+	ShortV2,
+};
