ef6186be5e5ed04bc2f99edd30f37671e87f2c24
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2015-03-03 14:19:05 +0100

    Enum parser

---
+++ Sharpineer/Sharpineer/Parser/Header/EnumInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpineer.Parser.Header
+{
+    public class EnumInfo
+    {
+        /// <summary>
+        /// Enum name
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Enum base type
+        /// </summary>
+        public string BaseType;
+
+        /// <summary>
+        /// Mapping from value name to actual value
+        /// </summary>
+        public readonly Dictionary<string, long> Values = new Dictionary<string, long>();
+
+        /// <summary>
+        /// List of enclosed namespaces
+        /// </summary>
+        public readonly List<string> Namespaces = new List<string>();
+    }
+}
--- Sharpineer/Sharpineer/Parser/Header/HeaderParserTest.cs
+++ Sharpineer/Sharpineer/Parser/Header/HeaderParserTest.cs
@@ -15,6 +15,57 @@ namespace Sharpineer.Parser.Header
         {
             var parser = new HeaderParser("data/EnumTestHeader.h");
             parser.Parse();
+
+            var enums = parser.Enums;
+
+            /*
+            enum FooE
+            {
+	            Entry1,
+	            Entry2,
+	            Entry4 = 4
+            };
+
+            enum class FooEC
+            {
+	            Val0,
+	            Val1,
+	            Val2
+            };
+
+            enum class FooECB : short
+            {
+	            ShortV0,
+	            ShortV1,
+	            ShortV2,
+            };
+            */
+
+            {
+                var e = enums.FirstOrDefault(ei => ei.Name == "FooE");
+                Assert.NotNull(e);
+
+                Assert.AreEqual(e.Name, "FooE");
+                Assert.AreEqual(e.BaseType, "int");
+
+                Assert.AreEqual(e.Values["Entry1"], 0L);
+                Assert.AreEqual(e.Values["Entry2"], 1L);
+                Assert.AreEqual(e.Values["Entry4"], 4L);
+            }
+
+            {
+                var e = enums.FirstOrDefault(ei => ei.Name == "FooEC");
+                Assert.NotNull(e);
+
+                Assert.That(e.Values.ContainsKey("Val1"));
+            }
+
+            {
+                var e = enums.FirstOrDefault(ei => ei.Name == "FooECB");
+                Assert.NotNull(e);
+
+                Assert.AreEqual(e.BaseType, "short");
+            }
         }
     }
 }
